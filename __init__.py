b4777bcaa59115b479bdf20c1b060b1bb078a3dc
Light-x02 <105165695+Light-x02@users.noreply.github.com>
2025-09-27 19:57:12 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .CropImage import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .image_metadata_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+WEB_DIRECTORY = "./js"
+
+# Rend les mappings accessibles à ComfyUI
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
