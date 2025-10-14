c268517b524f95ef6ea0cf9f191704d84bc4dead
Light-x02 <105165695+Light-x02@users.noreply.github.com>
2025-10-15 01:20:06 +0200

    Update LinkColorToggle.js

--- js/LinkColorToggle.js
+++ js/LinkColorToggle.js
@@ -129,7 +129,7 @@ import { app } from "/scripts/app.js";
             const options = ORIGINAL_MENU ? ORIGINAL_MENU.apply(this, arguments) : [];
             options.push(null);
             options.push({ content: "Lightx02", disabled: true });
-            options.push({ content: `${State.enabled ? "✓" : "☐"} Enable Link Colors (I/O)`, callback: () => toggle() });
+            options.push({ content: `${State.enabled ? "✓" : "☐"} 🎨 Link Colors (I/O)`, callback: () => toggle() });
             options.push(null);
             return options;
         };
@@ -144,3 +144,4 @@ import { app } from "/scripts/app.js";
         },
     });
 })();
+
